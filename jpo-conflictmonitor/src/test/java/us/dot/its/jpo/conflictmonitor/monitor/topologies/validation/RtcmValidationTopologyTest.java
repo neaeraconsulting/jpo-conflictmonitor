a1db19fabb66bdb86c5b397ecc4d34c277711ac0
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-09-14 23:02:08 -0600

    topology unit test

--- jpo-conflictmonitor/src/test/java/us/dot/its/jpo/conflictmonitor/monitor/topologies/validation/RtcmValidationTopologyTest.java
+++ jpo-conflictmonitor/src/test/java/us/dot/its/jpo/conflictmonitor/monitor/topologies/validation/RtcmValidationTopologyTest.java
@@ -8,13 +8,25 @@ import org.apache.kafka.streams.TopologyTestDriver;
 import org.junit.Test;
 import us.dot.its.jpo.conflictmonitor.monitor.algorithms.validation.rtcm.RtcmValidationParameters;
 import us.dot.its.jpo.conflictmonitor.monitor.algorithms.validation.spat.SpatValidationParameters;
+import us.dot.its.jpo.conflictmonitor.monitor.models.events.broadcast_rate.RtcmBroadcastRateEvent;
+import us.dot.its.jpo.conflictmonitor.monitor.models.events.minimum_data.RtcmMinimumDataEvent;
+import us.dot.its.jpo.conflictmonitor.monitor.serialization.JsonSerdes;
 import us.dot.its.jpo.conflictmonitor.monitor.topologies.timestamp_delta.SpatTimestampDeltaTopology;
+import us.dot.its.jpo.conflictmonitor.testutils.TopologyTestUtils;
 import us.dot.its.jpo.geojsonconverter.partitioner.RsuStationIdKey;
+import us.dot.its.jpo.geojsonconverter.pojos.ProcessedValidationMessage;
+import us.dot.its.jpo.geojsonconverter.pojos.geojson.Point;
 import us.dot.its.jpo.geojsonconverter.pojos.geojson.rtcm.ProcessedRTCM;
-import us.dot.its.jpo.geojsonconverter.serialization.JsonSerdes;
+import us.dot.its.jpo.geojsonconverter.pojos.geojson.rtcm.RTCMProperties;
 
 import java.time.Instant;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 @Slf4j
 public class RtcmValidationTopologyTest {
@@ -34,8 +46,8 @@ public class RtcmValidationTopologyTest {
     // Start time on 10-second window boundary
     final Instant startTime = Instant.ofEpochMilli(1674356320000L);
 
-    final int lowerBound = 90;
-    final int upperBound = 110;
+    final int lowerBound = 9;
+    final int upperBound = 11;
     final boolean debug = true;
 
     final String validationMsg = "Validation Message";
@@ -55,18 +67,44 @@ public class RtcmValidationTopologyTest {
 
         try (TopologyTestDriver driver = new TopologyTestDriver(topology, streamsConfig);
              Serde<ProcessedRTCM> processedRTCMSerde = us.dot.its.jpo.geojsonconverter.serialization.JsonSerdes.ProcessedRTCM();
-             Serde<RsuStationIdKey> keySerde = us.dot.its.jpo.geojsonconverter.serialization.JsonSerdes.RsuStationIdKey()) {
+             Serde<RsuStationIdKey> keySerde = us.dot.its.jpo.geojsonconverter.serialization.JsonSerdes.RsuStationIdKey();
+             Serde<RtcmMinimumDataEvent> minDataEventSerde = JsonSerdes.RtcmMinimumDataEvent();
+             Serde<RtcmBroadcastRateEvent> broadcastRateEventSerde = JsonSerdes.RtcmBroadcastRateEvent()) {
 
             var inputTopic = driver.createInputTopic(inputTopicName,
                     keySerde.serializer(), processedRTCMSerde.serializer());
+
+            var broadcastRateTopic = driver.createOutputTopic(broadcastRateTopicName,
+                    keySerde.deserializer(),  broadcastRateEventSerde.deserializer());
+
+            var minimumDataTopic = driver.createOutputTopic(minimumDataTopicName,
+                    keySerde.deserializer(), minDataEventSerde.deserializer());
 
             final RsuStationIdKey key = new RsuStationIdKey();
             key.setRsuId(rsuId);
             key.setStationId(stationId);
 
-
-
-        } finally {
+            // Send at .5 Hz (slow)
+            final int slowPeriodMillis = 2000;
+            final int totalTimeSeconds = 13;
+            List<Instant> instants = TopologyTestUtils.getInstants(startTime, slowPeriodMillis, totalTimeSeconds);
+            for (var currentInstant : instants) {
+                var map = createRtcm(currentInstant);
+                inputTopic.pipeInput(key, map, currentInstant);
+            }
+            var minDataList = minimumDataTopic.readKeyValuesToList();
+            assertThat("Should be > 1 min data events", minDataList, hasSize(greaterThan(1)));
+            for (var entry : minDataList) {
+                var resultKey = entry.key;
+                assertThat("min data key rsuId", resultKey.getRsuId(), equalTo(rsuId));
+                assertThat("min data key stationId", resultKey.getStationId(), equalTo(stationId));
+                var result = entry.value;
+                assertThat("min data event rsuId", result.getSource(), equalTo(source));
+                assertThat("min data stationId", result.getStationId(), equalTo(stationId));
+                assertThat("min data missingDataElements size", result.getMissingDataElements(), hasSize(1));
+                var msg = result.getMissingDataElements().getFirst();
+                assertThat("min data validation message match", msg, startsWith(validationMsg));
+            }
 
         }
 
@@ -99,5 +137,19 @@ public class RtcmValidationTopologyTest {
         parameters.setUpperBound(upperBound);
         parameters.setDebug(debug);
         return parameters;
+    }
+
+    private ProcessedRTCM createRtcm(Instant timestamp) {
+        var geometry = new Point(-105.0, 40.0);
+        var properties = new RTCMProperties();
+        properties.setOdeReceivedAt(timestamp.atZone(ZoneOffset.UTC));
+        properties.setCti4501Conformant(false);
+        properties.setStationId(stationId);
+        var valMsgList = new ArrayList<ProcessedValidationMessage>();
+        var msg = new ProcessedValidationMessage();
+        msg.setMessage(validationMsg);
+        valMsgList.add(msg);
+        properties.setValidationMessages(valMsgList);
+        return new ProcessedRTCM(geometry, properties);
     }
 }
