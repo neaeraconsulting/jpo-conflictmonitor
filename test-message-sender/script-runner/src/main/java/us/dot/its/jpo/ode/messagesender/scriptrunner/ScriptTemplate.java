a1ba98e5f157245c8e72e7344d1a0c60d39fb29d
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-12-28 23:28:09 -0700

    integration tests for rtcm minimum data events.  Update test message sender to handle rtcm timestamps

--- test-message-sender/script-runner/src/main/java/us/dot/its/jpo/ode/messagesender/scriptrunner/ScriptTemplate.java
+++ test-message-sender/script-runner/src/main/java/us/dot/its/jpo/ode/messagesender/scriptrunner/ScriptTemplate.java
@@ -21,7 +21,12 @@ public class ScriptTemplate {
     public static final String EPOCH_SECONDS = "\"@EPOCH_SECONDS@\"";
     public static final Pattern OFFSET_SECONDS = Pattern.compile("@OFFSET_SECONDS_(?<offset>-?[0-9.]+)@");
     public static final String TEMP_ID = "@TEMP_ID@";
-
+    public static final String YEAR = "\"@YEAR@\"";
+    public static final String MONTH = "\"@MONTH@\"";
+    public static final String DAY = "\"@DAY@\"";
+    public static final String HOUR = "\"@HOUR@\"";
+    public static final String MINUTE = "\"@MINUTE@\"";
+    public static final String OFFSET = "\"@OFFSET@\"";
 
 
     public static String fillTemplate(final Instant sendInstant, final String message, final String tempId) {
@@ -45,12 +50,25 @@ public class ScriptTemplate {
                 replaceIsoOffsetMillis(sendInstant,
                         replaceOffsetSeconds(epochSecond, message));
 
+        final int year = zdt.getYear();
+        final int month = zdt.getMonthValue();
+        final int day = zdt.getDayOfMonth();
+        final int hour = zdt.getHour();
+        final int minute = zdt.getMinute();
+        final long timeZoneOffsetMinutes = Math.round(zdt.getOffset().getTotalSeconds() / 60.0);
+
         return offsetsReplaced
                 .replace(ISO_DATE_TIME, isoDateTime)
                 .replace(MINUTE_OF_YEAR, Long.toString(minuteOfYear))
                 .replace(MILLI_OF_MINUTE, Long.toString(milliOfMinute))
                 .replace(EPOCH_SECONDS, String.format("%.3f", epochSecond))
-                .replace(TEMP_ID, tempId);
+                .replace(TEMP_ID, tempId)
+                .replace(YEAR, Integer.toString(year))
+                .replace(MONTH, Integer.toString(month))
+                .replace(DAY, Integer.toString(day))
+                .replace(HOUR, Integer.toString(hour))
+                .replace(MINUTE, Integer.toString(minute))
+                .replace(OFFSET, Long.toString(timeZoneOffsetMinutes));
 
 
     }
