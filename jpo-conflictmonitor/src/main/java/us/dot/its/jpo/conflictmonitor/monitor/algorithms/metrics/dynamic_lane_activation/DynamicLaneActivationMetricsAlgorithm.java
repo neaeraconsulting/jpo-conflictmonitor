ca25881afc3c72543a878b274650e04c48f7539c
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2026-01-19 16:22:55 -0700

    Creating dynamic lane metrics topology

--- jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/algorithms/metrics/dynamic_lane_activation/DynamicLaneActivationMetricsAlgorithm.java
+++ jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/algorithms/metrics/dynamic_lane_activation/DynamicLaneActivationMetricsAlgorithm.java
@@ -1,7 +1,10 @@
 package us.dot.its.jpo.conflictmonitor.monitor.algorithms.metrics.dynamic_lane_activation;
 
 import us.dot.its.jpo.conflictmonitor.monitor.algorithms.ConfigurableAlgorithm;
+import us.dot.its.jpo.conflictmonitor.monitor.algorithms.metrics.MetricsAlgorithmInterface;
 import us.dot.its.jpo.conflictmonitor.monitor.algorithms.revocable_enabled_lane_alignment.RevocableEnabledLaneAlignmentAlgorithm;
+import us.dot.its.jpo.conflictmonitor.monitor.models.metrics.dynamic_lane_activation.DynamicLaneActivationMetrics;
+import us.dot.its.jpo.geojsonconverter.partitioner.RsuIntersectionKey;
 
 /**
  * Dynamic Lane Activation Metrics Algorithm
@@ -9,5 +12,8 @@ import us.dot.its.jpo.conflictmonitor.monitor.algorithms.revocable_enabled_lane_
  * <p>Plugs into the {@link RevocableEnabledLaneAlignmentAlgorithm} as a subtopology</p>
  */
 public interface DynamicLaneActivationMetricsAlgorithm
-    extends ConfigurableAlgorithm<DynamicLaneActivationMetricsParameters> {
+    extends MetricsAlgorithmInterface<
+        DynamicLaneActivationMetrics,
+        RsuIntersectionKey,
+        DynamicLaneActivationMetricsParameters> {
 }
---
+++ jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/topologies/DynamicLaneActivationMetricsTopology.java
@@ -0,0 +1,46 @@
+package us.dot.its.jpo.conflictmonitor.monitor.topologies;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KStream;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+import us.dot.its.jpo.conflictmonitor.monitor.algorithms.BaseStreamsBuilder;
+import us.dot.its.jpo.conflictmonitor.monitor.algorithms.metrics.CommonMetricsParameters;
+import us.dot.its.jpo.conflictmonitor.monitor.algorithms.metrics.dynamic_lane_activation.DynamicLaneActivationMetricsParameters;
+import us.dot.its.jpo.conflictmonitor.monitor.algorithms.metrics.dynamic_lane_activation.DynamicLaneActivationMetricsStreamsAlgorithm;
+import us.dot.its.jpo.conflictmonitor.monitor.algorithms.metrics.dynamic_lane_activation.RevocableLaneStatus;
+import us.dot.its.jpo.conflictmonitor.monitor.models.metrics.dynamic_lane_activation.DynamicLaneActivationMetrics;
+import us.dot.its.jpo.geojsonconverter.partitioner.RsuIntersectionKey;
+
+import static us.dot.its.jpo.conflictmonitor.monitor.algorithms.metrics.dynamic_lane_activation.DynamicLaneActivationMetricsConstants.DEFAULT_DYNAMIC_LANE_ACTIVATION_METRICS_ALGORITHM;
+
+@Slf4j
+@Component(DEFAULT_DYNAMIC_LANE_ACTIVATION_METRICS_ALGORITHM)
+public class DynamicLaneActivationMetricsTopology
+    extends BaseStreamsBuilder<DynamicLaneActivationMetricsParameters>
+    implements DynamicLaneActivationMetricsStreamsAlgorithm {
+
+    private CommonMetricsParameters commonParameters;
+
+    @Override
+    protected Logger getLogger() {
+        return log;
+    }
+
+    @Override
+    public void setCommonParameters(CommonMetricsParameters parameters) {
+        this.commonParameters = parameters;
+    }
+
+    @Override
+    public CommonMetricsParameters getCommonParameters() {
+        return commonParameters;
+    }
+
+    @Override
+    public KStream<RsuIntersectionKey, DynamicLaneActivationMetrics>
+    buildTopology(StreamsBuilder builder, KStream<RsuIntersectionKey, RevocableLaneStatus> inputStream) {
+        return null;
+    }
+}
