a109ba6328165aadb508a9a88fa1b7161b22f36e
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2026-05-07 12:09:51 -0600

    fix record

--- jpo-conflictmonitor-batch-processing/src/main/java/us/dot/its/jpo/conflictmonitor/batch/models/atspm_spat/SignalGroupStatistics.java
+++ jpo-conflictmonitor-batch-processing/src/main/java/us/dot/its/jpo/conflictmonitor/batch/models/atspm_spat/SignalGroupStatistics.java
@@ -2,7 +2,6 @@ package us.dot.its.jpo.conflictmonitor.batch.models.atspm_spat;
 
 import lombok.Data;
 
-@Data
 public record SignalGroupStatistics (
     int signalGroup,
     double percentGreenPaired,
