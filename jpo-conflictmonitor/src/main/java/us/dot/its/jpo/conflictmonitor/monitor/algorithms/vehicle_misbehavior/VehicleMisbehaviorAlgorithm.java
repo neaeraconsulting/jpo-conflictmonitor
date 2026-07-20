b08c6914a160c929e9e714d2f2ab0d4722bcc7e9
john-wiens <jwiens3141@gmail.com>
2026-07-20 11:41:05 -0600

    Removing unused interface methods

--- jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/algorithms/vehicle_misbehavior/VehicleMisbehaviorAlgorithm.java
+++ jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/algorithms/vehicle_misbehavior/VehicleMisbehaviorAlgorithm.java
@@ -6,9 +6,6 @@ import us.dot.its.jpo.conflictmonitor.monitor.algorithms.Algorithm;
 
 public interface VehicleMisbehaviorAlgorithm
         extends Algorithm<VehicleMisbehaviorParameters>{
-
-    // void setAggregationAlgorithm(BsmMessageCountProgressionAggregationAlgorithm aggregationAlgorithm);
-
 }
 
 
