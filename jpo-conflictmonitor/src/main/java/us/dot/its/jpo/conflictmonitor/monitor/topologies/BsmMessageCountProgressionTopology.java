bf0c2baa3e7f038066952735d1fed7fde82ab732
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2026-08-04 18:36:48 -0600

    bsm message count progression fixes and unit tests

--- jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/topologies/BsmMessageCountProgressionTopology.java
+++ jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/topologies/BsmMessageCountProgressionTopology.java
@@ -18,6 +18,7 @@ import us.dot.its.jpo.conflictmonitor.monitor.algorithms.aggregation.bsm_message
 import us.dot.its.jpo.conflictmonitor.monitor.algorithms.bsm_message_count_progression.BsmMessageCountProgressionParameters;
 import us.dot.its.jpo.conflictmonitor.monitor.algorithms.bsm_message_count_progression.BsmMessageCountProgressionStreamsAlgorithm;
 
+import us.dot.its.jpo.conflictmonitor.monitor.models.bsm.BsmTimestampExtractor;
 import us.dot.its.jpo.geojsonconverter.partitioner.RsuIdPartitioner;
 import us.dot.its.jpo.geojsonconverter.partitioner.RsuLogKey;
 import us.dot.its.jpo.geojsonconverter.pojos.geojson.bsm.ProcessedBsm;
@@ -67,8 +68,9 @@ public class BsmMessageCountProgressionTopology
         );
         KStream<RsuLogKey, ProcessedBsm<Point>> inputStream = builder.stream(parameters.getBsmInputTopicName(),
                 Consumed.with(
-                        JsonSerdes.RsuLogKey(),
-                        JsonSerdes.ProcessedBsm()));
+                            JsonSerdes.RsuLogKey(),
+                            JsonSerdes.ProcessedBsm())
+                        .withTimestampExtractor(new BsmTimestampExtractor()));
 
 
         var eventStream = inputStream
